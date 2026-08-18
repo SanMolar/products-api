8b8227c0a25bbec6789bb59e95e428def90f9ec8
Victor Molar <alberto.molarcrz@gmail.com>
2026-08-17 20:04:48 -0600

    feat: add product read endpoints

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,17 @@ const app: Express = express();
 
 const port = 3000;
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const products: Product[] = [
+  { id: 1, name: "Teclado", price: 500 },
+  { id: 2, name: "Mouse", price: 300 },
+];
+
 app.get("/", (_req: Request, res: Response) => {
   res.send("Mi primera API está funcionando");
 });
@@ -11,12 +22,28 @@ app.get("/", (_req: Request, res: Response) => {
 app.get("/products", (_req: Request, res: Response) => {
   res.json({
     message: "Lista de productos",
-    products: [
-      { id: 1, name: "Teclado", price: 500 },
-      { id: 2, name: "Mouse", price: 300 },
-    ],
+    products: products,
   });
 });
+
+const getProductById = (
+  req: Request<{ receivedId: string }>,
+  res: Response,
+) => {
+  const receivedId = req.params.receivedId;
+  const numericId = Number(receivedId);
+
+  const productFound = products.find((product) => product.id === numericId);
+  console.log(productFound);
+  console.log(numericId, typeof numericId);
+  if (productFound === undefined){
+      return res.status(404).json({message: "Producto no encontrado"});
+
+    } 
+  res.json(productFound);
+};
+
+app.get("/products/:receivedId", getProductById);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
